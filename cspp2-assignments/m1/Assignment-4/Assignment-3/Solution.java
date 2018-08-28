6ed024a5c77d6f643fe0c7067188b1abd0bcb148
ramushetty <ummadishettyramu@msitprogram.net>
2018-08-28 23:26:56 +0530

    module 1 assignment 4

--- cspp2-assignments/m1/Assignment-4/Assignment-3/Solution.java
+++ cspp2-assignments/m1/Assignment-4/Assignment-3/Solution.java
@@ -1,16 +1,39 @@
 import java.util.Scanner;
+/**
+ * Class for solution.
+ * author ramu
+ * roll number 20186082
+ */
+public final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        //empty.
+    }
+    /**
+     * { var_description }.
+     */
+    public static final double NUM1 = 32, NUM2 = 1.8;
+    /**
+     * { main }.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        double degreesCelsius = scan.nextDouble();
+        degreestofare(degreesCelsius);
 
-public class Solution {
-	/*
-	Do not modify this main function.
-	*/
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		double degreesCelsius = scan.nextDouble();
-		calculateFromDegreesToFarenheit(degreesCelsius);
-	}
-	/*
-	Need to fill the calculateFromDegreesToFarenheit function and print the output
-	of fahrenheit.
-	*/
+    }
+
+    /**
+     * { called function}.
+     *
+     * @param      degree  The degree
+     */
+    public static void degreestofare(final double degree) {
+        System.out.println(degree * NUM2 + NUM1);
+
+    }
 }
