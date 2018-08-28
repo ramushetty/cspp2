992e7da9cfdaf316d4febc4fac69b3f2ba36bec4
ramushetty <ummadishettyramu@msitprogram.net>
2018-08-28 11:26:11 +0530

    helloworld

---
+++ cspp2-practice/m1/a10/HelloWorld.java
@@ -0,0 +1,19 @@
+/**
+first class in java.
+ */
+final class HelloWorld {
+    /** private constructor.
+    */
+    private HelloWorld() {
+        // abstraction
+    }
+     /** first method.
+     @param args of type string[]
+    */
+    public static void main(final String[] args) {
+
+        System.out.println("Hello, World!");
+    }
+}
+
+
