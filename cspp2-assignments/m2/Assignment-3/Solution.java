8924a57164a27b473047d9237fc3177a336d9b22
ramushetty <ummadishettyramu@msitprogram.net>
2018-08-28 20:43:57 +0530

    "check_style_score -> 4 of 5 passed. style score: 0/1 "

--- cspp2-assignments/m2/Assignment-3/Solution.java
+++ cspp2-assignments/m2/Assignment-3/Solution.java
@@ -6,23 +6,33 @@ public final class Solution {
     /*
     Do not modify this main function.
     */
+    /**
+     *
+     * @param      args  The arguments
+     */
     public static void main(final String[] args) {
         Scanner s = new Scanner(System.in);
         int base = s.nextInt();
         int exponent = s.nextInt();
-        int result = power(base,exponent);
+        int result = power(base, exponent);
         System.out.println(result);
     }
     /*
     Need to write the power function and print the output.
     */
     /**
-     * {called function }.
+     * { function_description }.
+     *
+     * @param      b     { parameter_description }
+     * @param      e     { parameter_description }
+     *
+     * @return     { description_of_the_return_value }
      */
+
     static int power(final int b, final int e) {
         if (e != 0) {
             return (b * power(b, e - 1));
-        }else {
+        } else {
             return 1;
         }
     }
