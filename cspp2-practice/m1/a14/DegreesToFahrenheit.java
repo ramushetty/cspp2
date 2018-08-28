af7872647345eb939d9e915ea94d5bd829167a8d
ramushetty <ummadishettyramu@msitprogram.net>
2018-08-28 11:32:30 +0530

    degrees to fahrenheit

---
+++ cspp2-practice/m1/a14/DegreesToFahrenheit.java
@@ -0,0 +1,25 @@
+/**
+    *degrees to fahrenheit.
+    *author: Ramu
+    *roll number : 20186082
+    *date : 27/08/2018
+*/
+public final class DegreesToFahrenheit {
+    /**
+        *DECLARING VARIABLE AND INITIALIZING.
+    */
+    public static final int NUM1 = 30, NUM2 = 32, NUM3 = 1.8;
+    //public static final int NUM2 =
+    /**
+    // empty constructor.
+    */
+    private DegreesToFahrenheit() {
+        // empty .
+    }
+    /**
+    @param args of type string[]
+    */
+    public static void main(final String[] args) {
+        System.out.println(NUM1 * NUM3 + NUM2);
+    }
+}
