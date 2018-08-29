2908f2be9288656aaa959eb6edbe348f0e0c483b
ramushetty <ummadishettyramu@msitprogram.net>
2018-08-29 14:27:53 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2-assignments/m3/Assignment-2 (1)/Assignment-2/Solution.java
+++ cspp2-assignments/m3/Assignment-2 (1)/Assignment-2/Solution.java
@@ -1,37 +1,49 @@
 
 import java.util.Scanner;
 /*
-	Do not modify this main function.
-	*/
+    Do not modify this main function.
+    */
 /**
  * Class for solution.
  * author ramu 
  * roll number 20186082
  * date 29/08/2018
  */
-public class Solution
-{
+public class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        // empty.
+    }
+    /**
+     * { var_description }.
+     */
+    public static final int NUM1 = 10;
+    /**
+     * { var_description }.
+     */
+    public static final int NUM2 = 7;
 /* Fill the main function to print the number of 7's between 1 to n*/
 /**
  * { function_description }.
  *
  * @param      args  The arguments
  */
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
 
-        Scanner s = new Scanner(System.in);      
+        Scanner s = new Scanner(System.in);
         int n = s.nextInt();
         int count = 0;
-        for (int i =1 ; i < n ; i++) {
-        	int j = i;
-        	while (j > 0) {
-        		int re = j % 10;
-        		if (re == 7) {
-        			count = count + 1;
-        		}
-        		j = j / 10;
-        	}
+        for (int i = 1 ;i < n ;i++) {
+            int j = i;
+            while (j > 0) {
+                int re = j % NUM1;
+                if (re == NUM2) {
+                    count = count + 1;
+                }
+                j = j / NUM1;
+            }
         }
 
     System.out.println(count);
