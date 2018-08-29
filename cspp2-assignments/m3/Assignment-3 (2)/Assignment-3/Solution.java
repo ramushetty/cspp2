a29c333a88945ab9f0b1d235ce103d2ad04a7594
ramushetty <ummadishettyramu@msitprogram.net>
2018-08-29 17:17:15 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2-assignments/m3/Assignment-3 (2)/Assignment-3/Solution.java
+++ cspp2-assignments/m3/Assignment-3 (2)/Assignment-3/Solution.java
@@ -1,32 +1,46 @@
 
 import java.util.Scanner;
 /*
-	Do not modify this main function.
-	*/
-public class Solution 
-{
+    Do not modify this main function.
+    */
+    /**
+     * Class for solution.
+     */
+public class Solution {
+    private Solution() {
+        //empty.
+    }
 
-    public static void main(String[] args)
-    {
+    /**
+     * { main function }.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(String[] args) {
 
-        Scanner s = new Scanner(System.in);      
+        Scanner s = new Scanner(System.in);
         int n1 = s.nextInt();
         int n2 = s.nextInt();
         int gcd = gcd(n1, n2);
         System.out.println(gcd);
     }
     /*
-	Need to write the gcd function and print the output.
-	*/
-    public static int gcd(final int n1, final int n2)
-    {
+    Need to write the gcd function and print the output.
+    */
+    /**
+     * { called function }.
+     *
+     * @param      n1    The n 1
+     * @param      n2    The n 2
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public static int gcd(final int n1, final int n2) {
         int k =0;
 
-        for (int i = 1; i<=n1; i++)
-        {
+        for (int i = 1; i<=n1; i++) {
 
-            if ((n1%i==0) && (n2%i==0))
-            {
+            if ((n1%i==0) && (n2%i==0)) {
 
                 k = i;
             } 
@@ -34,6 +48,5 @@ public class Solution
         }
         return k;
     }
-}     
-        
+}
 
