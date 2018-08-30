fcc2ff5ee8e3080882e4d0feac2664dfb1e69615
ramushetty <ummadishettyramu@msitprogram.net>
2018-08-30 19:02:45 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

--- cspp2-assignments/m4/Assignment-2/Assignment-2/Solution.java
+++ cspp2-assignments/m4/Assignment-2/Assignment-2/Solution.java
@@ -1,4 +1,4 @@
-import java.util.*;
+import java.util.Scanner;
 /**
  * Class for solution.
  * author ramu
@@ -23,8 +23,8 @@ public final class Solution {
 	int row = s.nextInt();
 	int col	= s.nextInt();
 	int[][] mat1 = new int[row][col];
-	for (int i = 0; i<mat1.length; i++) {
-		for (int j = 0; j<mat1.length; j++){
+	for (int i = 0; i < mat1.length; i++) {
+		for (int j = 0; j < mat1.length; j++){
 			mat1[i][j] = s.nextInt();
 		}
 	//System.out.println(Arrays.deepToString(mat1));
@@ -32,16 +32,20 @@ public final class Solution {
 	int row1 = s.nextInt();
 	int col1 = s.nextInt();
 	int[][] mat2 = new int[row1][col1];
-	for (int i = 0; i<mat2.length; i++) {
-		for (int j = 0; j<mat2.length; j++){
-			mat2[i][j] = s.nextInt();
+	if (mat1[row].length == mat2[col1].length) {
+		for (int i = 0; i < mat2.length; i++) {
+			for (int j = 0; j < mat2.length; j++){
+				mat2[i][j] = s.nextInt();
+			}
 		}
 	//System.out.println(Arrays.deepToString(mat2));
+	} else {
+		System.out.println("not possible");
 	}
 	if (mat1[row].length == mat2[col1].length) {
 		int[][] add = new int[row][col];
-		for (int i = 0; i<mat1.length; i++) {
-			for (int j = 0; j<mat2.length; j++) {
+		for (int i = 0; i < mat1.length; i++) {
+			for (int j = 0; j < mat2.length; j++) {
 				add[i][j] = mat1[i][j] + mat2[i][j];
 			}
 		}
