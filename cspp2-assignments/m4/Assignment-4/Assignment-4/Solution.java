cbfca90aec0d5495459dc7e1c2471b930ef6c4dc
ramushetty <ummadishettyramu@msitprogram.net>
2018-08-31 11:19:13 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2-assignments/m4/Assignment-4/Assignment-4/Solution.java
+++ cspp2-assignments/m4/Assignment-4/Assignment-4/Solution.java
@@ -7,34 +7,41 @@ import java.util.Scanner;
  * Class for solution.
  */
 public final class Solution {
-	private Solution() {
-		//empty.
-	}
-	/*
-	Do not modify the main function 
-	*/
-	/**
-	 * { function_description }.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args)
-	{
-		Scanner sc = new Scanner(System.in);
-		String s = sc.nextLine();
-		String reverse = reverseString(s);	
-		System.out.println(reverse);
-		
-	}
-	//Write reverseString function
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        //empty.
+    }
+    /*
+    Do not modify the main function
+    */
+    /**
+     * { function_description }.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        String reverse = reverseString(s);
+        System.out.println(reverse);
 
-	static String reverseString(final String str) {
-	//char[] arr = new char[str.length()];
-		String rev="";
-		for (int i = str.length() - 1; i >= 0; i--) {
-			rev = rev + str.charAt(i);
-		
-		}
-	return rev;
-	}
+    }
+    /**
+     * { function_description }.
+     *
+     * @param      str   The string
+     *
+     * @return     { description_of_the_return_value }
+     */
+    static String reverseString(final String str) {
+        //char[] arr = new char[str.length()];
+        String rev = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            rev = rev + str.charAt(i);
+
+        }
+        return rev;
+    }
 }
