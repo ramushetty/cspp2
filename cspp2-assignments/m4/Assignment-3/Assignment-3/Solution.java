d1c39b63ddc47fc770c31b284fa999e0ed11e2cb
ramushetty <ummadishettyramu@msitprogram.net>
2018-08-31 18:53:27 +0530

    "check_style_score -> 1 of 1 passed. style score: 0/1 "

--- cspp2-assignments/m4/Assignment-3/Assignment-3/Solution.java
+++ cspp2-assignments/m4/Assignment-3/Assignment-3/Solution.java
@@ -1,5 +1,7 @@
 
-// Given an String, Write a java method that returns the decimal value for the given binary string.
+// Given an String, Write a java method that returns the decimal
+// value for th
+ // given binary string.
 
 
 import java.util.Scanner;
@@ -7,42 +9,42 @@ import java.util.Scanner;
  * Class for solution.
  */
 public final class Solution { /*
-	Do not modify this main function.
-	*/
-	/**
-	 * Constructs the object.
-	 */
-	private Solution() {
-		//empty.
-	}
-	/**
-	 * { function_description }.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		for (int i = 0; i < n; i++) {
-			String s = sc.next();
-			int res = binaryToDecimal(s); 
-			System.out.println(res);
-		}
-	}
-	/**
-	 * { function_description }.
-	 *
-	 * @param      str   The string
-	 *
-	 * @return     { description_of_the_return_value }
-	 */
-	static int binaryToDecimal(final String str) {
-		String bi = str;
-		// for (i = 0;i<bi.length()-1;i++) {
+    Do not modify this main function.
+    */
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        //empty.
+    }
+    /**
+     * { function_description }.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            String s = sc.next();
+            int res = binaryToDecimal(s); 
+            System.out.println(res);
+        }
+    }
+    /**
+     * { function_description }.
+     *
+     * @param      str   The string
+     *
+     * @return     { description_of_the_return_value }
+     */
+    static int binaryToDecimal(final String str) {
+        String bi = str;
+        // for (i = 0;i<bi.length()-1;i++) {
 
-		// }
-		int i = Integer.parseInt(bi, 2);
-	return i;
-	}
+        // }
+        int i = Integer.parseInt(bi, 2);
+    return i;
+    }
 
 }
