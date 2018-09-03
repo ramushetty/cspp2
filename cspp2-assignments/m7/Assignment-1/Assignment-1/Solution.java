b552700ab5c9df0033f9d88355f76326417df580
ramushetty <ummadishettyramu@msitprogram.net>
2018-09-03 19:58:13 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2-assignments/m7/Assignment-1/Assignment-1/Solution.java
+++ cspp2-assignments/m7/Assignment-1/Assignment-1/Solution.java
@@ -3,43 +3,45 @@ import java.util.*;
  * Class for input validator.
  */
 class InputValidator {
-	String input;
-	/**
-	 * Constructs the object inputvalidator.
-	 *
-	 * @param      data  The data given input
-	 */
-	public InputValidator(String data) {
-		input = data;
-	}
-	/**
-	 * { checks the length of the given input string}.
-	 *
-	 * @return     { returns true or false }
-	 */
-	public boolean validateData() {
-		final int length = 6;
-		return input.length() >= length;
-	}
+    String input;
+    /**
+     * Constructs the object inputvalidator.
+     *
+     * @param      data  The data given input
+     */
+    public InputValidator(final String data) {
+        input = data;
+    }
+    /**
+     * { checks the length of the given input string}.
+     *
+     * @return     { returns true or false }
+     */
+    public boolean validateData() {
+        final int length = 6;
+        return input.length() >= length;
+    }
 }
 /**
  * { main class solution }.
  */
 public final class Solution {
-	/**
-	 * empty constructor.
-	 */
-	private Solution() {
-		//empty.
-	}
-	/**
-	 * { main function }.
-	 */
-	public static void main(final String args[]) {
-    	Scanner s = new Scanner(System.in);
-    	String input = s.next();
-    	InputValidator i = new InputValidator(input);    	
-    	System.out.println(i.validateData());
+    /**
+     * empty constructor.
+     */
+    private Solution() {
+        //empty.
+    }
+    /**
+     * { main function }
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner s = new Scanner(System.in);
+        String input = s.next();
+        InputValidator i = new InputValidator(input);
+        System.out.println(i.validateData());
 
     }
 
