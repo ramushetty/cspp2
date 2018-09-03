526180dd4385a670538afc29fe33a4daa65030c8
ramushetty <ummadishettyramu@msitprogram.net>
2018-09-03 19:55:56 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2-assignments/m7/Assignment-1/Assignment-1/Solution.java
+++ cspp2-assignments/m7/Assignment-1/Assignment-1/Solution.java
@@ -1,19 +1,41 @@
 import java.util.*;
-class InputValidator
-{
+/**
+ * Class for input validator.
+ */
+class InputValidator {
 	String input;
+	/**
+	 * Constructs the object inputvalidator.
+	 *
+	 * @param      data  The data given input
+	 */
 	public InputValidator(String data) {
 		input = data;
 	}
+	/**
+	 * { checks the length of the given input string}.
+	 *
+	 * @return     { returns true or false }
+	 */
 	public boolean validateData() {
-		int length = 6;
-		return input.length() > length;
+		final int length = 6;
+		return input.length() >= length;
 	}
 }
-public final class Solution
-{
-	public static void main(final String args[])
-    {
+/**
+ * { main class solution }.
+ */
+public final class Solution {
+	/**
+	 * empty constructor.
+	 */
+	private Solution() {
+		//empty.
+	}
+	/**
+	 * { main function }.
+	 */
+	public static void main(final String args[]) {
     	Scanner s = new Scanner(System.in);
     	String input = s.next();
     	InputValidator i = new InputValidator(input);    	
