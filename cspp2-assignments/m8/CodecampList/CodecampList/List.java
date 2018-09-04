79c7b5793bc7ce448f7831319d6525f0b5b5cb9c
ramushetty <ummadishettyramu@msitprogram.net>
2018-09-04 15:36:57 +0530

    "check_style_score -> 2 of 2 passed. style score: 0/1 "

--- cspp2-assignments/m8/CodecampList/CodecampList/List.java
+++ cspp2-assignments/m8/CodecampList/CodecampList/List.java
@@ -26,7 +26,6 @@ public class List {
      * This is a hard concept to understand. Discuss with your mentor.
      *
     */
-    
     // declare a private int[]
     // don't create the array yet using new
     // that's the job of the List constructor
@@ -72,7 +71,8 @@ public class List {
         // How many items do we have in the list when you create it?
         // An empty list has how many items?
         // That is the initial value to use for size.
-        arr = new int[10];
+        final int num = 10;
+        arr = new int[num];
         index = 0;
 
     }
@@ -88,18 +88,17 @@ public class List {
      * 
      * The method returns void (nothing)
      */
-    public void add(int item) {
+    public void add(final int item) {
         //Inserts the specified element at the end of the list.
         arr[index] = item;
         index++;
-    	
     }
 
     /*
      * The size method returns the value of the size.
      * The purpose of the method is to announce the size of the list
      * to the objects outside the list
-     * 
+     *
      * The method returns an int. Empty list should return 0.
      */
     public int size() {
@@ -111,7 +110,7 @@ public class List {
      * The remove method does what the name suggests.
      * Removes an int item, specified by the index argument, from the list
      * It also does an additional step.
-     * Think about what happens when 
+     * Think about what happens when
      * an item is removed from the middle of the list
      * It creates a hole in the list, right?
      * This would mean, all the items that are
@@ -127,19 +126,18 @@ public class List {
      * array = [1,3,0,0,0,0,0,0,0,0]
      * The method returns void (nothing)
      */
-    public void remove(int indx) {
+    public void remove(final int indx) {
         // write the logic for remove here.
         // Think about what to do to the size variable.
         if (indx > size()) {
         	System.out.println("Invalid Position Exception");
 
         } else {
-        	for (int i = indx; i < index -1; i++) {
-        		arr[i] = arr[i+1];
+        	for (int i = indx; i < index - 1; i++) {
+        		arr[i] = arr[i + 1];
         	}
         index--;
         }
-        
     }
 
     /*
@@ -150,10 +148,10 @@ public class List {
      * How can an element not be there at a given position?
      * Well, if the position is greater than the number of items
      * in the list then that would mean the item doesn't exist.
-     * How do we check if the position is greater than the 
+     * How do we check if the position is greater than the
      * number of items in the list? Would size variable be useful?
      */
-    public int get(int indx) {
+    public int get(final int indx) {
         // Replace the code below to write the code for get
         for (int i = 0; i < index; i++) {
         	if (i == indx) {
@@ -172,7 +170,6 @@ public class List {
      * System.out.println(l);
      * This statement is a shortcut for
      * System.out.println(l.toString());
-     * 
      * So, implement the toString method to display the items
      * in the list in the square brackets notation.
      * i.e., if the list has numbers 1, 2, 3
@@ -187,9 +184,10 @@ public class List {
 
         // Replace the code below
         String str = "[";
-        for (int i = 0; i < index-1; i++)
+        for (int i = 0; i < index - 1; i++) {
         	str += arr[i] + ",";
-        str += arr[index-1] + "]";
+        }
+        str += arr[index - 1] + "]";
         return str;
     } 
     
