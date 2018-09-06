12e8260594617cffde1eb0447aeb385646549cf6
ramushetty <ummadishettyramu@msitprogram.net>
2018-09-06 11:14:43 +0530

    "check_style_score -> 2 of 3 passed. style score: 0/1 "

--- cspp2-assignments/m10/Assignment-1/Assignment-1/List.java
+++ cspp2-assignments/m10/Assignment-1/Assignment-1/List.java
@@ -1,5 +1,6 @@
 import java.io.BufferedInputStream;
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class List {
 	//Implement all the methods mentioned to build a ListADT
@@ -111,7 +112,14 @@ public class List {
 	 */
 	public void add(int item) {
 		//Inserts the specified element at the end of the zelist.
+		if (size == list.length) {
+			resize();
+		}
 		list[size++] = item;
+	}
+	public void resize() {
+    	int[] copy = Arrays.copyOf(list, 2*list.length);
+    	list = copy;
 	}
 
 	/*
