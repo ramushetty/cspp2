9b1bbdaf9aec6dd7b45892a5bfd78e3043b0547d
ramushetty <ummadishettyramu@msitprogram.net>
2018-09-06 14:54:28 +0530

    "check_style_score -> 3 of 3 passed. style score: 0/1 "

--- cspp2-assignments/m10/Assignment-2/Assignment-2/Solution.java
+++ cspp2-assignments/m10/Assignment-2/Assignment-2/Solution.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 /**
  * Class for solution..
  */
-public class Solution {
+public final class Solution {
     /**
      * empty Constructor.
      */
@@ -19,7 +19,7 @@ public class Solution {
     public static void main(final String[] args) {
         // create an object of the list to invoke methods on it
         StringList sl = new StringList();
-        StringListInterface l = (StringListInterface) sl; 
+        StringListInterface l = (StringListInterface) sl;
         //Typecasting is done for StringListInterface
         // code to read the test cases input file
         Scanner stdin = new Scanner(new BufferedInputStream(System.in));
