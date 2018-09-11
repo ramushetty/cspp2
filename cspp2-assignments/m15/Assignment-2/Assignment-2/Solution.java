f630c06cd6c2393f8f2389ebe81eda938804e30c
ramushetty <ummadishettyramu@msitprogram.net>
2018-09-11 17:32:45 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

---
+++ cspp2-assignments/m15/Assignment-2/Assignment-2/Solution.java
@@ -0,0 +1,237 @@
+import java.util.Scanner;
+import java.io.BufferedInputStream;
+import java.util.Arrays;
+/**
+ * Exception for signaling set empty errors.
+ */
+class SetEmptyException extends Exception {
+	/**
+	 * Constructs the object.
+	 *
+	 * @param      msg   The message
+	 */
+	SetEmptyException(String msg) {
+		super(msg);
+	}
+}
+/**
+ * Exception for signaling invalid subset selection errors.
+ */
+class InvalidSubsetSelectionException extends Exception {
+	/**
+	 * Constructs the object.
+	 *
+	 * @param      msg   The message
+	 */
+	InvalidSubsetSelectionException(String msg) {
+		super(msg);
+	}
+}
+
+/**
+ * Class for sorted set adt.
+ */
+class SortedSetADT {
+	/**
+	 * { var_description }.
+	 */
+	private int[] set;
+	/**
+	 * { var_description }.
+	 */
+	private int size;
+	/**
+	 * { var_description }.
+	 */
+	private static final int NUM = 10;
+	SortedSetADT() {
+		set = new int[NUM];
+		size = 0;
+	}
+	/**
+	 * { function_description }.
+	 *
+	 * @param      item  The item
+	 */
+	public void add(final int item) {
+		if (size == set.length) {
+			resize();
+		}
+		set[size++] = item;
+	}
+	/**
+	 * { function_description }.
+	 */
+	public void resize() {
+		set = Arrays.copyOf(set, 2 * set.length);
+	}
+	/**
+	 * Adds all.
+	 *
+	 * @param      arr   The arr
+	 */
+	public void addAll(final int[] arr) {
+		if (size + arr.length > set .length) {
+			resize();
+		}
+		for (int i = 0; i < arr.length; i++) {
+			if (contains(arr[i]) == 0) {
+				add(arr[i]);
+			}
+		}
+		set = Arrays.copyOf(set, size);
+		Arrays.sort(set);
+
+	}
+	/**
+	 * { function_description }.
+	 *
+	 * @param      item  The item
+	 *
+	 * @return     { description_of_the_return_value }
+	 */
+	public int contains(final int item) {
+		for (int i = 0; i < size; i++) {
+			if (item == set[i]) {
+				return -1;
+			}
+
+		}
+		return 0;
+	}
+	/**
+	 * Returns a string representation of the object.
+	 *
+	 * @return     String representation of the object.
+	 */
+	public String toString() {
+		if (size == 0) {
+			return "{}";
+		}
+		int i = 0;
+		String str = "{";
+		for (i = 0; i < size - 1; i++) {
+			str = str + set[i] + "," + " ";
+		}
+		str = str + set[i] + "}";
+		return str;
+	}
+	/**
+	 * { function_description }.
+	 *
+	 * @param      end                The end
+	 *
+	 * @return     { description_of_the_return_value }
+	 *
+	 * @throws     SetEmptyException  { exception_description }
+	 */
+	public SortedSetADT headSet(final int end) throws SetEmptyException {
+		SortedSetADT sset = new SortedSetADT();
+		int count = 0;
+		for (int i = 0; i < set[end]; i++) {
+			if (set[i] < end) {
+				sset.add(set[i]);
+				count++;
+			} 
+		}
+		if (count  != 0) {
+			return sset;
+		}
+		throw new SetEmptyException("Set Empty Exception");
+		
+	}
+	/**
+	 * { function_description }.
+	 *
+	 * @return     { description_of_the_return_value }
+	 *
+	 * @throws     SetEmptyException  { exception_description }
+	 */
+	public int last() throws SetEmptyException {
+		if(size == 0) {
+			throw new SetEmptyException("Set Empty Exception");
+		}
+		return set[size - 1];
+	}
+	public SortedSetADT subSet(final int fromelement, int endelement) throws InvalidSubsetSelectionException {
+		if (fromelement > endelement || endelement < fromelement) {
+			throw new InvalidSubsetSelectionException("Invalid Arguments to Subset Exception");
+		}
+		SortedSetADT nset = new SortedSetADT();
+		for (int i = fromelement; i < endelement; i++) {
+			nset.add(set[i]);
+		}
+		return nset;
+	}
+}
+/**
+ * { item_description }.
+ */
+public final class Solution {
+	/**
+	 * Constructs the object.
+	 */
+	private Solution() {
+		//empty.
+	}
+	/**
+	 * { function_description }.
+	 *
+	 * @param      args  The arguments
+	 */
+	public static void main(String[] args) {
+		SortedSetADT s = new SortedSetADT();
+		Scanner stdin = new Scanner(new BufferedInputStream(System.in));
+		while (stdin.hasNext()) {
+			String line = stdin.nextLine();
+			String[] tokens = line.split(" ");
+			switch (tokens[0]) {
+				case "add":
+					if (tokens.length == 2) {
+						s.add(Integer.parseInt(tokens[1]));
+					}
+					break;
+				case "addAll":
+					if (tokens.length == 2) {
+						String[] t = tokens[1].split(",");
+						int[] a = new int[t.length];
+						for (int i = 0; i < a.length; i++) {
+							a[i] = Integer.parseInt(t[i]);
+						}
+						s.addAll(a);
+					}
+					break;
+				case "print":
+					System.out.println(s);
+					break;
+				case "headSet":
+					try {
+						System.out.println(s.headSet(Integer.parseInt(tokens[1])));
+					} catch(Exception ex) {
+						System.out.println("Set Empty Exception");
+					}
+					break;
+				case "last":
+					try {
+						System.out.println(s.last());
+					} catch (Exception ex) {
+						System.out.println("Set Empty Exception");
+					}
+					break;
+				case "subSet":
+					if (tokens.length == 2) {
+						String[] t = tokens[1].split(",");
+						try {
+						
+						SortedSetADT obj = s.subSet(Integer.parseInt(t[0]), Integer.parseInt(t[1]));
+						System.out.println(obj);
+						} catch (Exception ex) {
+							System.out.println("Invalid Arguments to subset Exception");
+						}
+					}
+					break;
+
+			}
+		}
+	}
+}
