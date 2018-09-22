244713dbdd7259cbb27409d3b87ee105ea777830
ramushetty <ummadishettyramu@msitprogram.net>
2018-09-22 17:39:13 +0530

    "check_style_score -> 1 of 3 passed. style score: 0/1 "

--- cspp2-assignments/m24/Bagofwords/Bagofwords/Solution.java
+++ cspp2-assignments/m24/Bagofwords/Bagofwords/Solution.java
@@ -1,10 +1,7 @@
 import java.util.Scanner;
-import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.io.File;
-import java.io.FileReader;
-import java.io.BufferedReader;
 import java.io.IOException;
 /**
  * Class for files.
